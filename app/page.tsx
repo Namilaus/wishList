68d68a24cfa531d208baf01e65a0aa29395944aa
namialus <sulaiman.atayi@hotmail.com>
2025-04-20 22:52:12 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@ import WishCard, { Wish } from "./WishCard";
 
 
 export default async function Home() {
-  const reqeust = await fetch(process.env.BACKEND_URL, {
-    cache:"no-cache"
+  
+  const url:string | undefined = process.env.BACKEND_URL;
+  let wishs:Wish[] = [];
+  
+  if(url){
+    const reqeust = await fetch(url , {
+      cache:"no-cache"
   })
-  const wishs:Wish[] = await reqeust.json();
+    wishs = await reqeust.json();
+  }
 
   return (
    <div className="flex"> { wishs.map((wish:Wish,i)=>(<WishCard key={i} data={wish} />)) }</div>
